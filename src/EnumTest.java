254848afdf3cac1b9f70bb39e883e2fc3a73d35e
matsushita <matsushita@aisl.cs.tut.ac.jp>
2020-05-07 11:42:33 +0900

    edit comments

--- src/EnumTest.java
+++ src/EnumTest.java
@@ -11,19 +11,25 @@ public class EnumTest {
 
 	static Scanner scanner = new Scanner(System.in);	//キーボード入力受付用
 	static String str;	//キーボード入力文字列格納
-	static String regex = "^[0-9]*$";	//半角数字列の正規表現
+	static String regex = "^[0-9]*$";	//正の半角数字列の正規表現
 	static Pattern p = Pattern.compile(regex);
 	static Matcher m;
 	static int year, month, day;	//年月
 	
 	public static void main(String[] args){
+		System.out.println("******** 星座判定プログラム ********");
+		
+		//各変数入力/設定
 		InputYear();
 		InputMonth();
 		setDayMax();
 		InputDay();
 		
+		//結果表示
 		System.out.println(year + "年" + month + "月" + day + "日"
 				+ "生まれのあなたの星座は" + Constellation.getType(year, month, day).getName() + "です");
+		
+		scanner.close();
 	}
 	
 	//年入力用関数
@@ -33,7 +39,7 @@ public class EnumTest {
 			str = scanner.next();
 			m = p.matcher(str);
 			if(!m.find()){
-				System.out.println("[Error]正の数字を入力して下さい");
+				System.out.println("[Error]自然数	を入力して下さい");
 				continue;
 			}
 			
@@ -49,7 +55,7 @@ public class EnumTest {
 			str = scanner.next();
 			m = p.matcher(str);
 			if(!m.find()){
-				System.out.println("[Error]正の数字を入力して下さい");
+				System.out.println("[Error]正の整数を入力して下さい");
 				continue;
 			}
 			
@@ -71,7 +77,7 @@ public class EnumTest {
 			str = scanner.next();
 			m = p.matcher(str);
 			if(!m.find()){
-				System.out.println("[Error]正の数字を入力して下さい");
+				System.out.println("[Error]正の整数を入力して下さい");
 				continue;
 			}
 			
@@ -89,8 +95,8 @@ public class EnumTest {
 	//誕生月の最終日を設定
 	static void setDayMax(){
 		Calendar c = Calendar.getInstance();
-		c.set(year, month - 1, 1);
-		c.add(Calendar.DATE, -1);
+		c.set(year, month, 1);	//翌月初め
+		c.add(Calendar.DATE, -1);	//その前日=当月末日
 		DAY_MAX = c.get(Calendar.DATE);
 	}
 }
